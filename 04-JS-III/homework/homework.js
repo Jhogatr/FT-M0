99977239980907928a62510c2eb537d7eca95e3d
jhoga@Jhogatr <jhogatr@gmail.com>
2022-01-04 18:24:53 -0500

    falta

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -320,6 +320,7 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+
 }
 
 function continueStatement(numero) {
@@ -329,6 +330,7 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  
 }
 
 
--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,14 +6,7 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
-var objeto = {
-    nombre: nombre,
-    edad: edad,
-    meow: function(){
-      return "Meow!"
-    }
-}
-return objeto
+
 }
 
 
@@ -22,8 +15,7 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-  objeto[property] = null
-  return objeto
+  
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -31,7 +23,7 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
-  objeto[metodo]()
+  
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
